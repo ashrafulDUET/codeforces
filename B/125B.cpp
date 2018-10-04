6fe119a582cee3ca48b15165e953bfbb40d18922
Md. Ashraful Haque <ashraful.duet1@gmail.com>
2018-10-04 23:04:27 +0600

    125B.cpp
    B. Simple XML
    
    Let's define a string <x> as an opening tag, where x is any small letter of the Latin alphabet. Each opening tag matches a closing tag of the type </x>, where x is the same letter.
    
    Tegs can be nested into each other: in this case one opening and closing tag pair is located inside another pair.
    
    Let's define the notion of a XML-text:
    
    an empty string is a XML-text
    if s is a XML-text, then s'=<a>+s+</a> also is a XML-text, where a is any small Latin letter
    if s1, s2 are XML-texts, then s1+s2 also is a XML-text
    You are given a XML-text (it is guaranteed that the text is valid), your task is to print in the following form:
    
    each tag (opening and closing) is located on a single line
    print before the tag 2 * h spaces, where h is the level of the tag's nestedness.
    Input
    The input data consists on the only non-empty string — the XML-text, its length does not exceed 1000 characters. It is guaranteed that the text is valid. The text contains no spaces.
    
    Output
    Print the given XML-text according to the above-given rules.
    
    Examples
    inputCopy
    <a><b><c></c></b></a>
    outputCopy
    <a>
      <b>
        <c>
        </c>
      </b>
    </a>
    inputCopy
    <a><b></b><d><c></c></d></a>
    outputCopy
    <a>
      <b>
      </b>
      <d>
        <c>
        </c>
      </d>
    </a>

---
+++ B/125B.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+stack<char>st;
+int main()
+{
+    string inp; int len;
+    cin>>inp;
+    len = inp.length();
+    for(int i=0;i<len;i+=3)
+    {
+        if(inp[i+1]!='/')
+        {
+            for(int j=0;j<2*(int)st.size() ; j++)
+                cout<<" ";
+            cout<<inp[i]<<inp[i+1]<<inp[i+2]<<endl;
+            st.push(inp[i+1]);
+        }
+        else
+        {
+            st.pop();
+            for(int j=0;j<2*(int)st.size() ; j++)
+                cout<<" ";
+            cout<<inp[i]<<inp[i+1]<<inp[i+2]<<inp[i+3]<<endl;
+            i++;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
